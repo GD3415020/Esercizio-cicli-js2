cfeb33260450cadd3101505be2d58d30acb8e323
GDIsanto <logantom85@gmail.com>
2025-11-19 18:29:00 +0100

    terminato esercizio

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log(`ciao`);
+let somma = 0;
+let count = 0;
+let min = parseInt(prompt(`inserisci il numero di partenza`));
+let max = parseInt(prompt(`inserisci il numero di arrivo`));
+
+for(let i=min; i<=max; i++){
+    if(i % 2 == 0){
+        console.log(i);
+    } else{
+        somma = somma + i;
+        count++;
+    }
+}
+
+console.log(`la somma del valore dei numeri dispari da ${min} a ${max} è pari a ${somma} e la loro media è ${somma / count}`);
